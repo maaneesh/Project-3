00535db35ab36ef6543632b1487a62219f50a728
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-09-07 12:41:04 -0400

    setup server using express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = 5006
+
+//initialized express
+const app = express()
+
+
+app.listen(port, ()=>{
+    console.log(`Server started on port: ${port}`);
+})
